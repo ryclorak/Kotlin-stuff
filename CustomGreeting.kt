a0ec008f45b7b0ca8e00cdc4f2e806eb7b8b4965
Karol <ryclorak@gmail.com>
2020-06-24 22:43:06 -0700

    finished
    simple script for a custom greeting, accepting user input

--- CustomGreeting.kt
+++ CustomGreeting.kt
@@ -1,7 +1,10 @@
 fun GreetUser(name: String = "user"){	//default name is user
-	println("Hello $name!");			//$ for inserting var into string
+	println("Hello $name!");						//$ for inserting var into string
 	println("Good morning.");
 }
 
 fun main(args: Array<String>) {
-	print("Yo what's your name?");
+	print("Yo what's your name?\n>>");
+	var name = readLine()!!; 						//var to make it mutable, !! to ensure not null
+	println();													//newline after getting input
+	GreetUser(name);	
