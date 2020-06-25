fc4088b864aa796411996df1137dbb71871b0c45
Karol <ryclorak@gmail.com>
2020-06-24 23:01:38 -0700

    conciseness and arrays
    exploring more possible ways to use the language

--- CustomGreeting.kt
+++ CustomGreeting.kt
@@ -7,4 +7,20 @@ fun main(args: Array<String>) {
 	print("Yo what's your name?\n>>");
 	var name = readLine()!!; 						//var to make it mutable, !! to ensure not null
 	println();													//newline after getting input
-	GreetUser(name);	
+	GreetUser(name);
+
+	print("\n\n");
+
+	GreetUser(name = readLine()!!);			//it works, yay conciseness
+
+	//trying arrays and loops with kotlin, need to test the following code
+
+	print("How many names do you want to add to the name array?\n>>")
+	var num = readLine()!!.toInt();
+	var nameArr: Array<String>(num);
+
+	for (int i=0; i<num: i++)
+		nameArr[i] = readLine()!!;				//TODO: look for ways
+
+		//TODO: GreetUser using the array in a separate function
+}	
